fec0b86afe34a487d12b84061dc3bb26f93e8fd1
Hafiz <a@a.com>
2023-05-09 09:30:09 +0400

    my todo

---
+++ mytodo/js/script.js
@@ -0,0 +1,64 @@
+// const input = document.querySelector('input');
+// const btn = document.querySelector('button');
+// const ol = document.querySelector('ol');
+// const listcount = document.querySelector('.listcount');
+
+// const todoapp = (e) => {
+//     e.preventDefault;
+//     if (!input.value || input.value.trim().length === 0) {
+//         alert("please fill input");
+//     } else {
+//         let li = document.createElement("li");
+//         li.innerHTML = `<span> ${input.value} <button class ="del"> delete</button>  </span>`;
+//         ol.appendChild(li);
+//         input.value = "";
+
+
+
+//         let list = document.querySelectorAll('li');
+
+
+//         for (let i = 0; i < list.length; i++) {
+
+//             document.querySelectorAll(".del")[i].onclick = () => {
+//                 list[i].remove();
+
+
+//             }
+//             listcount.innerHTML = i + 1;
+
+//         }
+
+//     }
+
+// }
+// btn.onclick = todoapp;
+
+
+
+
+
+// document.querySelector('.clear').onclick=()=>{
+
+//     let list =document.querySelectorAll('li');
+  
+//     for (let index = 0; index < list.length; index++) {
+     
+//       list[index].remove();
+    
+     
+     
+
+//     }
+  
+//   }
+
+
+
+
+
+
+const mymethod=()=>{
+
+
+}
